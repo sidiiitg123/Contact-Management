b6db0e0d564a21453d9faae52b4c2a5f9cb26a34
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2021-03-16 22:34:43 +0530

    bootstrap added closing #4

---
+++ src/main/java/com/spring/smartcontact/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.spring.smartcontact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("smart","this is smart contact manager");
+
+        return "home";
+    }
+    @RequestMapping("/base")
+    public String base(){
+
+        return "base";
+    }
+}

25ec9dd5157739b3955cc22b08cee3c2603dcd8c
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2021-03-18 11:16:52 +0530

    showing contact in very effective manner Closing #16

--- src/main/java/com/spring/smartcontact/controller/UserController.java
+++ src/main/java/com/spring/smartcontact/controller/UserController.java
@@ -4,6 +4,7 @@ package com.spring.smartcontact.controller;
 import com.spring.smartcontact.helper.Message;
 import com.spring.smartcontact.model.Contact;
 import com.spring.smartcontact.model.User;
+import com.spring.smartcontact.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
@@ -19,31 +20,37 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    private  UserRepository userRepository;
+    private UserRepository userRepository;
+    private ContactRepository contactRepository;
 
     @Autowired
-    public void setUserRepository(UserRepository userRepository)
-    {
+    public void setContactRepository(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     //method for adding common data to response
     @ModelAttribute
-    public void addCommonData(Model model,Principal principal){
-        String username=principal.getName();
-        User user=userRepository.getUserByEmail(username);//username is email here
-        model.addAttribute("user",user);
+    public void addCommonData(Model model, Principal principal) {
+        String username = principal.getName();
+        User user = userRepository.getUserByEmail(username);//username is email here
+        model.addAttribute("user", user);
     }
 
 
     //dashboard home
     @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal){
+    public String dashboard(Model model, Principal principal) {
 
 
         return "normal/user_dashboard";
@@ -51,52 +58,69 @@ public class UserController {
 
     @GetMapping("/test")
     @ResponseBody
-    public String test(){
-        User user=new User();
+    public String test() {
+        User user = new User();
         user.setEmail("sudhanshu457@gmail.com");
         user.setName("sid");
-       userRepository.save(user);
+        userRepository.save(user);
 
-       return "success";
+        return "success";
     }
 
     //open add form handler
     @GetMapping("/add-contact")
-    public String openAddContactForm(Model model){
+    public String openAddContactForm(Model model) {
 
-        model.addAttribute("title","Add contact");
-        model.addAttribute("contact",new Contact());
+        model.addAttribute("title", "Add contact");
+        model.addAttribute("contact", new Contact());
         return "normal/add_contact_form";
     }
 
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file, Principal principal, HttpSession session){
-        try{
-            String username=principal.getName();
-            User user=userRepository.getUserByEmail(username);
+    public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file, Principal principal, HttpSession session) {
+        try {
+            String username = principal.getName();
+            User user = userRepository.getUserByEmail(username);
 
             //processing and uploading file..
-            if(file.isEmpty()){
+            if (file.isEmpty()) {
                 System.out.println("file is empty");
-            }else{
+            } else {
                 //upload file to folder
                 contact.setImage(file.getOriginalFilename());
-                File saveFile=new ClassPathResource("static/img").getFile();
-                Path path=Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
-                Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+                File saveFile = new ClassPathResource("static/img").getFile();
+                Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
                 System.out.println("Image is uploaded");
             }
             contact.setUser(user);
             user.getContacts().add(contact);
             userRepository.save(user);
-            session.setAttribute("message",new Message("Contact is inserted successfully!!","alert-success"));
+            session.setAttribute("message", new Message("Contact is inserted successfully!!", "alert-success"));
             System.out.println(contact);
-        }catch (Exception e){
-            session.setAttribute("message",new Message("error in inserting contact!!","alert-danger"));
+        } catch (Exception e) {
+            session.setAttribute("message", new Message("error in inserting contact!!", "alert-danger"));
             System.out.println("ERROR" + e.getMessage());
             e.printStackTrace();
         }
 
         return "normal/add_contact_form";
     }
+
+    //show contact handler
+    @GetMapping("/show-contacts")
+    public String showContacts(Model model,Principal principal) {
+        model.addAttribute("title", "user contacts");
+
+        String username = principal.getName();
+        User user = userRepository.getUserByEmail(username);
+
+        List<Contact> contacts= contactRepository.findContactByUser(user.getId());
+
+        model.addAttribute("contacts",contacts);
+
+        return "normal/show_contacts";
+    }
+
+
 }
